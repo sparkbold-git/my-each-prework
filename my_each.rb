99e1b23e84168fc30e83797cd4ee039c89d0ff68
sparkbold-git <sparkboldstudio@gmail.com>
2018-08-24 15:42:00 -0500

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(words)
+  if words.size > 0
+  i = 0
+  array = []
+  while i < words.size
+    yield words[i]
+    array << words [i]
+    i+= 1
+  end
+else
+  "This block should not run!"
+end
+array
 end
